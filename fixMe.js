7451f4dcc23acd63d2aacc1509c81f6fae6dc38a
nass92 <goujanassim@hotmail.com>
2021-02-25 16:51:17 +0100

    ewe

---
+++ fixMe.js
@@ -0,0 +1,16 @@
+let list = ['Alice', 'Bob', 'Charlie', 'Craig', 'Eva']
+for (let i = 0; i <= list.length; ++i) {
+
+  switch (list[i]) {
+    case 'Alice':
+      console.log('Alice want to exchange with Bob')
+    case 'BOB':
+      console.log('Bob want to exchange with Alice')
+    case 'CHARLIE':
+      console.log('Charlie is a generic third participant')
+    case 'CRAIG':
+      console.log('Craig is a password cracker')
+    case 'EVE':
+      console.log('Eve is an eavesdropper')
+  }
+}
---
+++ welcome.js
@@ -0,0 +1,5 @@
+let name = 'Alice'
+let gender = 'female'
+let s = gender === 'male' ? `Bonjour ${name}, vous êtes entré sur le chan` : `Bonjour ${name}, vous êtes entrée sur le chan`
+console.log(`${s}`)
+
